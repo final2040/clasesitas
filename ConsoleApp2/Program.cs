f2b32491ae66affb2b7009d1bec30e0560a6da67
final20 <final20@yamiel-dash.cloud>
2026-07-04 17:22:45 -0600

    Initialize new .NET 9.0 console application with string manipulation examples

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ClaseCadenas;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string cadena = "HOLA MUNDO";
+        Console.WriteLine(cadena);
+
+        // Recorrer una cadena en orden inverso
+        for (int i = cadena.Length - 1; i >= 0; i = i - 1)
+        {
+            Console.Write(cadena[i]);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine(Revertir(cadena));
+        Console.WriteLine(AMinusculas(cadena));
+    }
+
+    public static string AMinusculas(string texto)
+    {
+        string resultado = "";
+        for(int i = 0; i < texto.Length; i++)
+        {
+            if(texto[i] >= 'A' && texto[i] <= 'Z')
+            {
+                resultado += (char)(texto[i] + 32);
+            }
+            else
+            {
+                resultado += texto[i];
+            }
+        }
+
+        return resultado;
+    }
+
+    public static string Revertir(string texto)
+    {
+        string resultado = "";
+        for (int i = texto.Length - 1; i >= 0; i--)
+        {
+            resultado += texto[i];
+            // resultado = resultado + texto[i];
+        }
+
+        return resultado;
+    }
+}
