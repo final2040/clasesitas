32e4e6d5038ad871ed06ad9eba0032cd7d43ee21
final20 <final20@yamiel-dash.cloud>
2026-07-17 22:08:23 -0600

    Refactor `Program.cs` by reorganizing method definitions and simplifying logic flow

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,50 +1,9 @@
-﻿using System;
+﻿namespace ClaseCadenas;
 
 class Program
 {
-    public static string ExtraerSubcadena(string texto, int inicio, int longitud)
+    static void Main(string[] args)
     {
-        if (texto == null || texto.Length == 0)
-            return "";
-
-        if (inicio < 0 || longitud < 0)
-            return "";
-
-        if (inicio >= texto.Length)
-            return "";
-
-        if (inicio + longitud > texto.Length)
-            longitud = texto.Length - inicio;
-
-        char[] resultado = new char[longitud];
-
-        for (int i = 0; i < longitud; i++)
-            resultado[i] = texto[inicio + i];
-
-        return new string(resultado);
-    }
-
-    static void Main()
-    {
-        string opcion = "";
-
-        while (opcion != "n")
-        {
-            Console.Write("Texto: ");
-            string texto = Console.ReadLine();
-
-            Console.Write("Inicio: ");
-            int inicio = int.Parse(Console.ReadLine());
-
-            Console.Write("Longitud: ");
-            int longitud = int.Parse(Console.ReadLine());
-
-            string resultado = ExtraerSubcadena(texto, inicio, longitud);
-
-            Console.WriteLine("Resultado: " + resultado);
-            Console.WriteLine();
-        }
-
         string cadena = "HOLA MUNDO";
         Console.WriteLine(cadena);
 
@@ -59,24 +18,12 @@ class Program
         Console.WriteLine(AMinusculas(cadena));
     }
 
-    public static string Revertir(string texto)
-    {
-        string resultado = "";
-        for (int i = texto.Length - 1; i >= 0; i--)
-        {
-            resultado += texto[i];
-            // resultado = resultado + texto[i];
-        }
-
-        return resultado;
-    }
-
     public static string AMinusculas(string texto)
     {
         string resultado = "";
-        for (int i = 0; i < texto.Length; i++)
+        for(int i = 0; i < texto.Length; i++)
         {
-            if (texto[i] >= 'A' && texto[i] <= 'Z')
+            if(texto[i] >= 'A' && texto[i] <= 'Z')
             {
                 resultado += (char)(texto[i] + 32);
             }
@@ -84,6 +31,18 @@ class Program
             {
                 resultado += texto[i];
             }
+        }
+
+        return resultado;
+    }
+
+    public static string Revertir(string texto)
+    {
+        string resultado = "";
+        for (int i = texto.Length - 1; i >= 0; i--)
+        {
+            resultado += texto[i];
+            // resultado = resultado + texto[i];
         }
 
         return resultado;
